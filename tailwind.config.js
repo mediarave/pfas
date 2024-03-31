d9521b50d5908b866dea300764e0eee9f80f051a
Alok Mishra <alok@alokmishra.com>
2024-03-30 21:32:49 -0400

    schedule design started for pfas

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,28 +3,23 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-    content: [
-        './wp-content/themes/aim/*.{html,js,php}',
-        './wp-content/themes/aim/templates/*.php',
-        './wp-content/themes/aim/mediarave/*.php',
-        './wp-content/themes/aim/layouts/*.{html,js,php}'
-    ],
+    content: ['./*.{html,js}'],
     safelist: ['hidden'],
     theme: {
         extend: {
             colors: {
-                teal: colors.teal
+                teal: colors.teal,
             },
             maxWidth: {
-                xxs: '16rem'
-            }
-        }
+                xxs: '16rem',
+            },
+        },
     },
     plugins: [],
 
     corePlugins: {
         backgroundOpacity: false,
         textOpacity: false,
-        preflight: false
-    }
+        preflight: false,
+    },
 };
